9d62ba7dcaba5b74c5fd5ae27f0ffee20532bb9f
Esmeralda (AKA root) <ebarbacil33@gmail.com>
2018-10-21 12:05:11 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,19 +3,26 @@ from sys import argv
 from .src.training import training
 from .test.testData import testData
 from .test.predictions import predictions
+import multiprocessing
 
-train_dataset_fp = argv[1]
-test_fp = argv[2]
+def run_tensorflow():
+    training(tf,model,ds_entrenamiento)
+    testData(tf,ds_test,model)
+    predictions(tf,model)
+
+
+ds_entrenamiento = argv[1]
+ds_test = argv[2]
+
 
 tf.enable_eager_execution()
 
-model = tf.keras.Sequential([#see this numbers...
-      tf.keras.layers.Dense(10, activation="relu", input_shape=(4,)),
+model = tf.keras.Sequential([#jugar con estos numeros si los resultados no dan bien...
+      tf.keras.layers.Dense(10, activation="relu", input_shape=(3,)),
       tf.keras.layers.Dense(10, activation="relu"),
-      tf.keras.layers.Dense(3)
+      tf.keras.layers.Dense(33) #Cantidad de resutados
     ])
 
-
-training(tf,model,train_dataset_fp)
-testData(tf,test_fp,model)
-predictions(tf,model)
+p = multiprocessing.Process(target=run_tensorflow)
+p.start()
+p.join()
